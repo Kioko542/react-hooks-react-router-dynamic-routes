90acb2af5b28283e03e8c0eeda1157cb38f37e19
Emmanue <kioxmanu7@gmail.com>
2023-10-31 18:05:04 +0300

    assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
-import MoviesPage from "./MoviesPage";
+import MoviesPage from "./MoviesPage"; // Import your MoviesPage component
 
 function App() {
   const [movies, setMovies] = useState([
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
+import MovieShow from "./MovieShow";
 import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+
+      {/* 
+        Update the Route component to pass the `movies` prop to MovieShow
+        and ensure the MovieShow component receives the `movieId` parameter
+      */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
